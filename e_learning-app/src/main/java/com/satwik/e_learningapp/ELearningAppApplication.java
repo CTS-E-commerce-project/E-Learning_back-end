90945e192a24be2ee8afd49722bcca024dc6efb1
satwik <satwikkunal8825@gmail.com>
2024-04-03 15:23:00 +0530

    backend body

---
+++ e_learning-app/src/main/java/com/satwik/e_learningapp/ELearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.satwik.e_learningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningAppApplication.class, args);
+	}
+
+}
---
+++ e_learning-app/src/test/java/com/satwik/e_learningapp/ELearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satwik.e_learningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
